b6d9cb3097e42e433e61f07511239dabfe51a44a
Pawel Cieslak <pcieslak4480@gmail.com>
2025-09-11 21:47:59 +0000

    Homework #1 submission

--- src/app.cpp
+++ src/app.cpp
@@ -4,8 +4,27 @@
 #include <string>
 using namespace std;
 
-bool isPrime(int x);
-int closestPrime(int x);
+bool isPrime(int x){
+    if (x<2) 
+        return false;
+
+    for (int i=2; i<x; i++) {
+       if (x%i == 0)
+          return false;
+    }
+    return true;
+}
+
+int closestPrime(int x){
+    if (isPrime(x)) 
+        return x; 
+
+    int num = x+1;
+    while(!isPrime(num)){
+        num++;
+    }
+    return num;
+}
 /** You should put in a main for testing, but then comment it out for submission.
  * The test.cpp file will include the main in your submission. You do not edit the test.cpp 
  * file. The only file to edit is app.cpp**/
